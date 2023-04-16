01cc6673c068af057b81faae7df72f50852a7dbb
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2023-04-16 13:17:09 +0200

    feat: Refactor course component to show if is empty
    - Rename "Added courses" to "Tested courses"
    - Add a computed value to check if the course list is empty

--- src/components/Admin/Courses.vue
+++ src/components/Admin/Courses.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 
 import { inject } from 'vue'
-import { NList, NSpin, NScrollbar } from 'naive-ui';
+import { NList, NSpin, NScrollbar, NEmpty, NButton } from 'naive-ui';
 import CourseItem from './Courses/CourseItem.vue';
 import type { CourseView } from '@/interfaces';
 import type { API } from '@/api';
@@ -9,7 +9,7 @@ import type { API } from '@/api';
 
 export default {
   components: {
-    NList, NSpin, NScrollbar,
+    NList, NSpin, NScrollbar, NEmpty, NButton,
     CourseItem
   },
   mounted() {
@@ -20,28 +20,41 @@ export default {
       this.loading = false;
       this.courses = courses;
     }).catch(err => {
+      this.loading = false;
       console.error(err);
-    })
+    });
   },
   data() {
     return {
       loading: true,
       courses: [] as CourseView[]
     }
-  }
+  },
+  computed: {
+    isEmpty() : boolean {
+      return this.courses.length === 0;
+    } 
+  } 
 }
 </script>
 
 
 <template>
   <div class="container">
-    <h2>Added courses</h2>
+    <h2>Tested courses</h2>
     <n-scrollbar>
       <n-spin :show="loading" style="min-height: 200px;">
         <n-list hoverable clickable>
           <!-- add a CourseItem for each course -->
           <CourseItem v-for="course in courses" :course="course" />
         </n-list>
+        <n-empty description="No e-learning courses found :(" v-if="!loading && isEmpty">
+          <template #extra>
+            <n-button size="small">
+              Start by adding a course! <!-- TODO take me to a tutorial page -->
+            </n-button>
+          </template>
+        </n-empty>
       </n-spin>
     </n-scrollbar>
   </div>
