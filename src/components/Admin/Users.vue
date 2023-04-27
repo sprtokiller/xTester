110a86bfb36edd17a3112736a4d4104bc701d4b7
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2023-04-27 14:27:01 +0200

    feat: Refactor Users.vue for API and loading state usage
    - Implement API and message injection in Admin Users component
    - Refactor mounted method to use injected API and message functions
    - Introduce a loading state for data properties
    - Add CourseItem component to courses list in template

--- src/components/Admin/Users.vue
+++ src/components/Admin/Users.vue
@@ -1,27 +1,64 @@
 <script lang="ts">
 
-import { RouterLink } from 'vue-router'
+import { inject } from 'vue'
+import { NSpin, NButton, useMessage, NH2 } from 'naive-ui';
+import type { CourseView } from '@/interfaces';
+import type { API } from '@/services/api';
 
 export default {
-  // components: {
-    
-  // },
-  // methods: {
-  //   handleUpdateValue (key: string, item: MenuOption) {
-  //     }
-  // },
-  // mounted() {
-  //   console.log("Menu");
-  // },
-  // data() {
-  //   return {
-  //     menuOptions: menu
-  //   }
-  // }
+  components: {
+    NSpin, NButton, NH2
+  },
+  setup () {
+    const MSG = useMessage();
+    const API = inject('API') as API;
+    return { MSG, API }
+  },
+  mounted() {
+    // fetch users and user groups from the API
+    this.API.getCourseList().then(courses => {
+      this.loading = false;
+    }).catch(err => {
+      this.loading = false;
+      this.MSG.error(err.message);
+    });
+  },
+  data() {
+    return {
+      loading: true as boolean
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
 }
 </script>
 
 
 <template>
-  <p>I am Users</p>
+  <div class="container">
+    <!-- <CourseDetail v-if="detail" :courseID="detail" @close-detail="closeDetail" @open-detail="showDetail" :key="reloadKey"/>
+
+    <div v-else>
+      <n-h2>List of courses</n-h2>
+      <n-scrollbar>
+        <n-spin :show="loading" style="min-height: 200px;">
+          <n-list hoverable clickable>
+            <!-/- add a CourseItem for each course -/->
+            <CourseItem v-for="course in courses" :course="course" @open-detail="showDetail"/>
+          </n-list>
+          <n-empty description="No e-learning courses found :(" v-if="!loading && isEmpty">
+            <template #extra>
+              <n-button size="small">
+                Start by adding a course! <!-/- TODO: take me to a tutorial page -/->
+              </n-button>
+            </template>
+          </n-empty>
+        </n-spin>
+      </n-scrollbar>
+    </div> -->
+  </div> 
 </template>
