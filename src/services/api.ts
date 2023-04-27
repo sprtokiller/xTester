d65bb0814df7a49eed4566623001c54b759bf8ca
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2023-04-27 14:32:34 +0200

    feat: Refactor API service object and add session management.
    - Update API base URL to `http://localhost:7331/api/` with `withCredentials` property
    - Add `login`, `getSession`, and `setSession` methods to API interface

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,35 @@
 import axios from 'axios';
 import type { CourseView, TestView, CourseDetail } from '@/interfaces';
 import { mockCourses, mockTests, mockCourseDetails } from '@/_mock';
+
 const api = axios.create({
-  baseURL: 'https://api.example.com/',
+  baseURL: 'http://localhost:7331/api/',
+  withCredentials: true
 });
 
-
 export interface API {
+    login(googleCredential: string): Promise<void>;
+    getSession(): Promise<void>;
+    setSession(): Promise<void>;
+
     getCourseList(): Promise<CourseView[]>;
     getTestList(): Promise<TestView[]>;
     getCourseDetail(courseId: number): Promise<CourseDetail>;
   }
 
 export const API = {
+  login(googleCredential: string) {
+    return api.post('/user/login', { googleCredential });
+  },
+
+  getSession() {
+    return api.get('/user/get');
+  },
+
+  setSession() {
+    return api.get('/user/set');
+  },
+
   getCourseList() { // TODO: Real API
     //return api.get(url, config);
     //return promise resolve after 1 second
