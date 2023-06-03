a37f3361721bb527ecdc7ba5e6e969cf39e106b9
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2023-06-03 08:04:34 +0200

    Feat: Added n-input for groups

--- src/views/Admin/Tests/TestAddView.vue
+++ src/views/Admin/Tests/TestAddView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { useMessage, NButton, NH3, NSpin, NCard, NForm, NFormItem, NInput, NSelect, NRadioGroup, NRadioButton, NDatePicker } from 'naive-ui'
+import { ref, onMounted, h } from 'vue'
+import type { Ref } from 'vue'
+import { useMessage, NButton, NH3, NSpin, NCard, NForm, NFormItem, NInput, NSelect, NRadioGroup, NRadioButton, NDatePicker, NIcon } from 'naive-ui'
 import type { FormInst, SelectOption, FormRules, FormItemRule } from 'naive-ui'
 import { useRouter } from 'vue-router'
+import { PersonFilled, PersonOffFilled } from '@vicons/material'
 
 import BackButton from '@/components/Admin/BackButton.vue'
 import LoadingHeader from '@/components/Admin/LoadingHeader.vue'
@@ -13,22 +15,24 @@ const router = useRouter()
 const MSG = useMessage()
 const API = useApi()
 
-const loading = ref(true)
-const error = ref(false)
-const uploading = ref(false)
+const loading: Ref<boolean> = ref(true)
+const error: Ref<boolean> = ref(false)
+const uploading: Ref<boolean> = ref(false)
 
-const formRef = ref<FormInst | null>(null)
-const formValue = ref({
+const formRef: Ref<FormInst | null> = ref(null)
+const formValue: Ref<IFormData> = ref({
   courseUUID: null,
   name: '',
+  groupUUIDs: [],
   endType: 'MANUAL',
   startAt: new Date().getTime(),
   startAtEndAt: [new Date().getTime(), new Date().getTime() + 7 * 24 * 60 * 60 * 1000]
-} as IFormData)
+})
 
 interface IFormData {
   courseUUID: string | null,
   name: string,
+  groupUUIDs: string[],
   endType: EndType,
   startAt: number
   startAtEndAt: [number, number]
@@ -45,6 +49,12 @@ const rules: FormRules = {
     message: 'Please enter a name for the test',
     trigger: ['input', 'blur']
   },
+  groupUUIDs: {
+    type: 'array',
+    required: true,
+    message: 'Please select at least one group',
+    trigger: ['input', 'blur']
+  },
   endType: {
     required: true,
     message: 'Please select an end type',
@@ -57,7 +67,7 @@ const rules: FormRules = {
   },
   startAtEndAt: {
     type: 'array',
-    validator (rule: FormItemRule, value: [number, number]) {
+    validator(rule: FormItemRule, value: [number, number]) {
       return value[1] > new Date().getTime() && value[1] > value[0]
     },
     message: 'The test must end in the future',
@@ -65,7 +75,31 @@ const rules: FormRules = {
   }
 }
 
-const courseOptions = ref<SelectOption[]>([])
+/**
+ *               <span style="color: var(--gray-3); margin-left: 0.5rem;">
+                {{ group.groupTestersCount }}
+                <n-icon size="medium">
+                  <PersonFilled style="vertical-align: bottom !important;" />
+                </n-icon>
+              </span>
+ */
+
+function renderLabel(option: SelectOption) {
+  return [
+    option.label as string,
+    h('span', { style: { color: 'var(--gray-3)', marginLeft: '0.5rem' } }, [
+      (option as any).nCount,
+      h(NIcon, {size: 'medium', style: { verticalAlign: '-0.15em' } }, { default: () => h(PersonFilled) }),
+    ]),
+    h('span', { style: { color: 'var(--gray-3)', marginLeft: '0.5rem' } }, [
+      (option as any).aCount,
+      h(NIcon, {size: 'medium', style: { verticalAlign: '-0.15em' } }, { default: () => h(PersonOffFilled) }),
+    ]),
+  ]
+}
+
+const courseOptions: Ref<SelectOption[]> = ref([])
+const groupOptions: Ref<SelectOption[]> = ref([])
 
 // define props
 const props = defineProps({
@@ -116,8 +150,18 @@ function rewriteStartAtEndAt() {
 onMounted(async () => {
   try {
     loading.value = true
+    // get the list of groups TODO: store
+    groupOptions.value = (await API.getGroupList()).map((group) => {
+      return {
+        label: group.groupName,
+        value: group.groupUUID,
+        nCount: group.groupTestersCount,
+        aCount: group.groupAnonymousCount
+      }
+    })
+    // get the list of courses
     const courses: ICourseView[] = await API.getCourseList()
-    courseOptions.value = courses.map((course) => {
+    courseOptions.value = (courses).map((course) => {
       return {
         label: course.name + ' (ver. ' + course.version + ', ' + course.author + ')',
         value: course.courseUUID
@@ -164,6 +208,10 @@ onMounted(async () => {
               <n-form-item path="name" label="Test name">
                 <n-input v-model:value="formValue.name" placeholder="Test Name" />
               </n-form-item>
+              <n-form-item path="groupUUIDs" label="Groups">
+                <n-select v-model:value="formValue.groupUUIDs" filterable multiple :options="groupOptions"
+                  :render-label="renderLabel" placeholder="Select groups" />
+              </n-form-item>
               <n-form-item path="endType" label="When should the test end?">
                 <div class="d-flex flex-row flex-wrap align-items-center">
                   <n-radio-group v-model:value="formValue.endType" name="RBG1" style="margin-right: 1rem;">
@@ -178,11 +226,11 @@ onMounted(async () => {
                 </div>
               </n-form-item>
               <n-form-item v-if="formValue.endType == 'MANUAL'" path="startAt" label="Test will start at:">
-                <n-date-picker v-model:value="formValue.startAt" type="datetime" v-on:change="rewriteStartAtEndAt"/>
-                  <!-- TestAddView.vue:128 [naive/date-picker]: `on-change` is deprecated, please use `on-update:value` instead. //TODO: --> 
+                <n-date-picker v-model:value="formValue.startAt" type="datetime" v-on:change="rewriteStartAtEndAt" />
+                <!-- TestAddView.vue:128 [naive/date-picker]: `on-change` is deprecated, please use `on-update:value` instead. //TODO: -->
               </n-form-item>
               <n-form-item v-else path="startAtEndAt" label="Test will be accesible during:">
-                <n-date-picker v-model:value="formValue.startAtEndAt" type="datetimerange" v-on:change="rewriteStartAt"/>
+                <n-date-picker v-model:value="formValue.startAtEndAt" type="datetimerange" v-on:change="rewriteStartAt" />
               </n-form-item>
             </n-form>
 
